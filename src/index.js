d2356f1321b1e4cc4228af9cfe35e51603a79438
Ignacio <martin.ignacio@hotmail.com>
2017-01-17 02:02:42 -0300

    testing git

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar  />
         <VideoList videos={this.state.videos} />
       </div>
     )
