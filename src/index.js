47e904aa44b373c2b200f61729aeb8bb73c11c3d
Ignacio Martin <martin.ignacio@hotmail.com>
2017-01-18 09:26:09 -0300

    video player done and working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyBnL3206f9Y-Eb2v619tGgfPiG8SFbbT5g';
 
@@ -10,18 +12,34 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+     };
 
-    YTSearch({key: API_KEY, term:'surfboards'}, (videos) => {
-      this.setState({ videos });
-    });
+    this.videoSearch('surfboards');
   }
 
+ videoSearch(term) {
+   YTSearch({key: API_KEY, term: term}, (videos) => {
+     this.setState({
+       videos: videos,
+       selectedVideo: videos[0]
+     });
+   });
+ };
+
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar  />
-        <VideoList videos={this.state.videos} />
+        <SearchBar onVideoSearch={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+         />
       </div>
     )
   };
